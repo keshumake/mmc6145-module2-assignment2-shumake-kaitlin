b11d36abc98f5b8ddd7c788aa71ae24e7f910abe
Kaitlin Shumake <shumakek@ufl.edu>
2023-02-03 21:31:49 -0500

    commented out empty attributes

--- src/App.jsx
+++ src/App.jsx
@@ -46,16 +46,16 @@ export default function App() {
         // add time, bestTime, previousTime props
         openModal={() => setShowModal(true)}
         
-        time={}
-        bestTime={}
-        previousTime={}
+        //time={}
+        //bestTime={}
+        //previousTime={}
       />
       <CardGame
         // add onGameStart, onGameEnd props
         cardTexts={cardTexts}
 
-        onGameStart={}
-        onGameEnd={}
+        //onGameStart={}
+        //onGameEnd={}
 
       />
       <Modal isShown={showModal} close={() => setShowModal(false)} />
